d33dce8970c0a6031b478bb5f7bd312200beaef8
Milton Calderon <mvcalderonc@correo.udistrital.edu.co>
2020-11-23 20:01:09 -0500

    se ajusta el código de dependencia_id

--- database/scripts/20201119_114703_insertar_datos_tablas_parametricas_up.sql
+++ database/scripts/20201119_114703_insertar_datos_tablas_parametricas_up.sql
@@ -22,17 +22,17 @@ INSERT INTO solicitud.tipo_observacion(id, nombre, descripcion, codigo_abreviaci
 INSERT INTO solicitud.tipo_observacion(id, nombre, descripcion, codigo_abreviacion, numero_orden, fecha_creacion, fecha_modificacion, activo)VALUES (2, 'Alerta', 'Indica que la observación es una alerta', 'alt', 2, now(), now(), true);
 
 --Inserta los estados para las solicitudes de producción docente para la oficina de docencia
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (1, 1, 1, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (2, 1, 2, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (3, 1, 3, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (4, 1, 4, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (5, 1, 5, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (6, 1, 6, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (7, 1, 7, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (8, 1, 8, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (9, 1, 9, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (10, 1, 10, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (11, 1, 11, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (12, 1, 12, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (13, 1, 13, 1, 3, now(), now(), true);
-INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (14, 1, 14, 1, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (1, 1, 1, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (2, 1, 2, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (3, 1, 3, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (4, 1, 4, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (5, 1, 5, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (6, 1, 6, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (7, 1, 7, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (8, 1, 8, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (9, 1, 9, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (10, 1, 10, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (11, 1, 11, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (12, 1, 12, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (13, 1, 13, 46, 3, now(), now(), true);
+INSERT INTO solicitud.estado_tipo_solicitud(id, tipo_solicitud_id, estado_id, dependencia_id, numero_dias, fecha_creacion, fecha_modificacion, activo)VALUES (14, 1, 14, 46, 3, now(), now(), true);
