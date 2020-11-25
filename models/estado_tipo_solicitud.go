04832ff238eadd48fc728b251597e12b44177604
Milton Calderon <mvcalderonc@correo.udistrital.edu.co>
2020-11-24 21:09:17 -0500

    se ajusta para que consulte las entidades relacionadas

--- models/estado_tipo_solicitud.go
+++ models/estado_tipo_solicitud.go
@@ -53,7 +53,7 @@ func GetEstadoTipoSolicitudById(id int) (v *EstadoTipoSolicitud, err error) {
 func GetAllEstadoTipoSolicitud(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(EstadoTipoSolicitud))
+	qs := o.QueryTable(new(EstadoTipoSolicitud)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
