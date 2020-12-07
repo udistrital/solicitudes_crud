4420d051a1382d647a6c012aebb19cab69b08d2d
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-12-07 11:21:43 -0500

    fix: se corrige el orden de estados en el archivo de subida de migracion para actualizacion de estados

--- database/scripts/20201203_184105_actualizacion_definicion_estados_up.sql
+++ database/scripts/20201203_184105_actualizacion_definicion_estados_up.sql
@@ -1,14 +1,14 @@
---Se actualizan 14 -> 3
+--Se actualizan 14 -> 2
 update solicitud.solicitud_evolucion_estado
-set estado_tipo_solicitud_id_anterior = 3
+set estado_tipo_solicitud_id_anterior = 2
 where estado_tipo_solicitud_id_anterior = 14;
 
 update solicitud.solicitud_evolucion_estado
-set estado_tipo_solicitud_id = 3
+set estado_tipo_solicitud_id = 2
 where estado_tipo_solicitud_id = 14;
 
 update solicitud.solicitud
-set estado_tipo_solicitud_id = 3
+set estado_tipo_solicitud_id = 2
 where estado_tipo_solicitud_id = 14;
 
 --Se actualizan 7 -> 4
@@ -98,8 +98,8 @@ where id > 9;
 
 --Se actualizan los estados
 update solicitud.estado set nombre = 'Radicada', codigo_abreviacion = 'RAD' where id = 1;
-update solicitud.estado set nombre = 'Para evaluación', codigo_abreviacion = 'PEV' where id = 2;
-update solicitud.estado set nombre = 'Requiere modificación', codigo_abreviacion = 'RMO' where id = 3;
+update solicitud.estado set nombre = 'Requiere modificación', codigo_abreviacion = 'RMO' where id = 2;
+update solicitud.estado set nombre = 'Para evaluación', codigo_abreviacion = 'PEV' where id = 3;
 update solicitud.estado set nombre = 'Verificada', codigo_abreviacion = 'VER' where id = 4;
 update solicitud.estado set nombre = 'En espera par', codigo_abreviacion = 'EEP' where id = 5;
 update solicitud.estado set nombre = 'Agrupada', codigo_abreviacion = 'AGR' where id = 6;
