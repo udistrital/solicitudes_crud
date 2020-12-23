b721804e3050e2a116aa44482bf69fb0d520bc05
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-12-23 14:09:57 -0500

    feat: se realizan pequeños ajustes para conexion con sga_mid

--- models/tr_paquete.go
+++ models/tr_paquete.go
@@ -8,18 +8,18 @@ import (
 )
 
 type TrPaquete struct {
-	Paquete                *Paquete
-	SolicitudesPaquete     *[]TrSolicitudPaquete
+	Paquete            *Paquete
+	SolicitudesPaquete *[]TrSolicitudPaquete
 }
 
 type TrSolicitudPaquete struct {
-	PaqueteSolicitud       *PaqueteSolicitud
+	PaqueteSolicitud *PaqueteSolicitud
 	//EstadoSolicitudPaquete *EstadoTipoSolicitud
-	Observaciones          *[]Observacion
-	EvolucionesEstado      *[]SolicitudEvolucionEstado
+	Observaciones     *[]Observacion
+	EvolucionesEstado *[]SolicitudEvolucionEstado
 }
 
-func AddNuevoPaquete(m *TrPaquete) (err error){
+func AddNuevoPaquete(m *TrPaquete) (err error) {
 	o := orm.NewOrm()
 	err = o.Begin()
 
@@ -36,7 +36,7 @@ func AddNuevoPaquete(m *TrPaquete) (err error){
 			v.PaqueteSolicitud.FechaCreacion = time_bogota.TiempoBogotaFormato()
 			v.PaqueteSolicitud.FechaModificacion = time_bogota.TiempoBogotaFormato()
 
-			if _, errTr = o.Insert(v.PaqueteSolicitud); errTr != nil{
+			if _, errTr = o.Insert(v.PaqueteSolicitud); errTr != nil {
 				err = errTr
 				fmt.Println(err)
 				_ = o.Rollback()
@@ -70,11 +70,11 @@ func AddNuevoPaquete(m *TrPaquete) (err error){
 						evolucionEstado.EstadoTipoSolicitudId = v2.EstadoTipoSolicitudId
 					}
 
-					v2.FechaLimite = time_bogota.TiempoBogotaFormato()
+					// v2.FechaLimite = time_bogota.TiempoBogotaFormato()
 					v2.FechaCreacion = time_bogota.TiempoBogotaFormato()
 					v2.FechaModificacion = time_bogota.TiempoBogotaFormato()
 
-					evolucionEstado.FechaLimite = time_bogota.TiempoBogotaFormato()
+					// evolucionEstado.FechaLimite = time_bogota.TiempoBogotaFormato()
 					evolucionEstado.FechaCreacion = time_bogota.TiempoBogotaFormato()
 					evolucionEstado.FechaModificacion = time_bogota.TiempoBogotaFormato()
 
@@ -106,47 +106,48 @@ func AddNuevoPaquete(m *TrPaquete) (err error){
 				}
 			}
 
-			for _, v2 := range *v.Observaciones{
-				var observacion Observacion
+			for _, v2 := range *v.Observaciones {
+				if v2.Activo {
+					var observacion Observacion
 
-				if errTr = o.QueryTable(new(Observacion)).RelatedSel().Filter("SolicitudId__Id", v.PaqueteSolicitud.SolicitudId.Id).One(&observacion); err == nil {
+					if errTr = o.QueryTable(new(Observacion)).RelatedSel().Filter("SolicitudId__Id", v.PaqueteSolicitud.SolicitudId.Id).One(&observacion); err == nil {
 
-					if observacion.TipoObservacionId != v2.TipoObservacionId {
-						observacion.TipoObservacionId = v2.TipoObservacionId
-					}
+						if observacion.TipoObservacionId != v2.TipoObservacionId {
+							observacion.TipoObservacionId = v2.TipoObservacionId
+						}
 
-					if observacion.TerceroId != v2.TerceroId {
-						observacion.TerceroId = v2.TerceroId
-					}
+						if observacion.TerceroId != v2.TerceroId {
+							observacion.TerceroId = v2.TerceroId
+						}
 
-					if observacion.Valor != v2.Valor {
-						observacion.Valor = v2.Valor
-					}
-					if v2.Id != 0 {
-						observacion.FechaModificacion = time_bogota.TiempoBogotaFormato()
-						if _, errTr = o.Update(&observacion, "TipoObservacionId", "TerceroId", "Valor", "FechaModificacion"); errTr != nil {
-							err = errTr
-							fmt.Println(err)
-							_ = o.Rollback()
-							return
+						if observacion.Valor != v2.Valor {
+							observacion.Valor = v2.Valor
+						}
+						if v2.Id != 0 {
+							observacion.FechaModificacion = time_bogota.TiempoBogotaFormato()
+							if _, errTr = o.Update(&observacion, "TipoObservacionId", "TerceroId", "Valor", "FechaModificacion"); errTr != nil {
+								err = errTr
+								fmt.Println(err)
+								_ = o.Rollback()
+								return
+							}
+						} else {
+							v2.SolicitudId = v.PaqueteSolicitud.SolicitudId
+							if _, errTr = o.Insert(&v2); errTr != nil {
+								err = errTr
+								fmt.Println(err)
+								_ = o.Rollback()
+								return
+							}
 						}
 					} else {
-						v2.SolicitudId = v.PaqueteSolicitud.SolicitudId
-						if _, errTr = o.Insert(&v2); errTr != nil {
-							err = errTr
-							fmt.Println(err)
-							_ = o.Rollback()
-							return
-						}
+						err = errTr
+						fmt.Println(err)
+						_ = o.Rollback()
+						return
 					}
-				} else {
-					err = errTr
-					fmt.Println(err)
-					_ = o.Rollback()
-					return
 				}
 			}
-
 		}
 		_ = o.Commit()
 	} else {
@@ -184,7 +185,7 @@ func UpdatePaquete(m *TrPaquete) (err error) {
 						}
 					} else {
 						v2.PaqueteSolicitud.PaqueteId.Id = int(v.Id)
-						if _, errTr = o.Insert(&v2.PaqueteSolicitud); errTr != nil{
+						if _, errTr = o.Insert(&v2.PaqueteSolicitud); errTr != nil {
 							err = errTr
 							fmt.Println(err)
 							_ = o.Rollback()
@@ -192,92 +193,92 @@ func UpdatePaquete(m *TrPaquete) (err error) {
 						}
 					}
 				}
-/*	
-				for _, v3 := range *v2.EvolucionesEstado {
-					var evolucionEstado SolicitudEvolucionEstado
-					if errTr = o.QueryTable(new(SolicitudEvolucionEstado)).RelatedSel().Filter("SolicitudId__Id", v2.PaqueteSolicitud.SolicitudId.Id).One(&evolucionEstado); err == nil {
-	
-						if evolucionEstado.TerceroId != v3.TerceroId {
-							evolucionEstado.TerceroId = v3.TerceroId
-						}
-	
-						if evolucionEstado.EstadoTipoSolicitudIdAnterior != v3.EstadoTipoSolicitudIdAnterior {
-							evolucionEstado.EstadoTipoSolicitudIdAnterior = v3.EstadoTipoSolicitudIdAnterior
-						}
-	
-						if evolucionEstado.EstadoTipoSolicitudId != v3.EstadoTipoSolicitudId {
-							evolucionEstado.EstadoTipoSolicitudId = v3.EstadoTipoSolicitudId
-						}
-	
-						if evolucionEstado.FechaLimite != v3.FechaLimite {
-							evolucionEstado.FechaLimite = v3.FechaLimite
-						}
-	
-						if v3.Id != 0 {
-							if _, errTr = o.Update(&evolucionEstado, "TerceroId", "EstadoTipoSolicitudIdAnterior", "EstadoTipoSolicitudId", "FechaLimite", "FechaModificacion"); errTr != nil {
-								err = errTr
-								fmt.Println(err)
-								_ = o.Rollback()
-								return
+				/*
+					for _, v3 := range *v2.EvolucionesEstado {
+						var evolucionEstado SolicitudEvolucionEstado
+						if errTr = o.QueryTable(new(SolicitudEvolucionEstado)).RelatedSel().Filter("SolicitudId__Id", v2.PaqueteSolicitud.SolicitudId.Id).One(&evolucionEstado); err == nil {
+
+							if evolucionEstado.TerceroId != v3.TerceroId {
+								evolucionEstado.TerceroId = v3.TerceroId
+							}
+
+							if evolucionEstado.EstadoTipoSolicitudIdAnterior != v3.EstadoTipoSolicitudIdAnterior {
+								evolucionEstado.EstadoTipoSolicitudIdAnterior = v3.EstadoTipoSolicitudIdAnterior
+							}
+
+							if evolucionEstado.EstadoTipoSolicitudId != v3.EstadoTipoSolicitudId {
+								evolucionEstado.EstadoTipoSolicitudId = v3.EstadoTipoSolicitudId
+							}
+
+							if evolucionEstado.FechaLimite != v3.FechaLimite {
+								evolucionEstado.FechaLimite = v3.FechaLimite
+							}
+
+							if v3.Id != 0 {
+								if _, errTr = o.Update(&evolucionEstado, "TerceroId", "EstadoTipoSolicitudIdAnterior", "EstadoTipoSolicitudId", "FechaLimite", "FechaModificacion"); errTr != nil {
+									err = errTr
+									fmt.Println(err)
+									_ = o.Rollback()
+									return
+								}
+							} else {
+								v3.SolicitudId = v2.PaqueteSolicitud.SolicitudId
+								if _, errTr = o.Insert(&v3); errTr != nil {
+									err = errTr
+									fmt.Println(err)
+									_ = o.Rollback()
+									return
+								}
 							}
 						} else {
-							v3.SolicitudId = v2.PaqueteSolicitud.SolicitudId
-							if _, errTr = o.Insert(&v3); errTr != nil {
-								err = errTr
-								fmt.Println(err)
-								_ = o.Rollback()
-								return
-							}
+							err = errTr
+							fmt.Println(err)
+							_ = o.Rollback()
+							return
 						}
-					} else {
-						err = errTr
-						fmt.Println(err)
-						_ = o.Rollback()
-						return
 					}
-				}
-	
-				for _, v3 := range *v2.Observaciones{
-					var observacion Observacion
-	
-					if errTr = o.QueryTable(new(Observacion)).RelatedSel().Filter("SolicitudId__Id", v2.PaqueteSolicitud.SolicitudId.Id).One(&observacion); err == nil {
-	
-						if observacion.TipoObservacionId != v3.TipoObservacionId {
-							observacion.TipoObservacionId = v3.TipoObservacionId
-						}
-	
-						if observacion.TerceroId != v3.TerceroId {
-							observacion.TerceroId = v3.TerceroId
-						}
-	
-						if observacion.Valor != v3.Valor {
-							observacion.Valor = v3.Valor
-						}
-						if v3.Id != 0 {
-							observacion.FechaModificacion = time_bogota.TiempoBogotaFormato()
-							if _, errTr = o.Update(&observacion, "TipoObservacionId", "TerceroId", "Valor", "FechaModificacion"); errTr != nil {
-								err = errTr
-								fmt.Println(err)
-								_ = o.Rollback()
-								return
+
+					for _, v3 := range *v2.Observaciones{
+						var observacion Observacion
+
+						if errTr = o.QueryTable(new(Observacion)).RelatedSel().Filter("SolicitudId__Id", v2.PaqueteSolicitud.SolicitudId.Id).One(&observacion); err == nil {
+
+							if observacion.TipoObservacionId != v3.TipoObservacionId {
+								observacion.TipoObservacionId = v3.TipoObservacionId
+							}
+
+							if observacion.TerceroId != v3.TerceroId {
+								observacion.TerceroId = v3.TerceroId
+							}
+
+							if observacion.Valor != v3.Valor {
+								observacion.Valor = v3.Valor
+							}
+							if v3.Id != 0 {
+								observacion.FechaModificacion = time_bogota.TiempoBogotaFormato()
+								if _, errTr = o.Update(&observacion, "TipoObservacionId", "TerceroId", "Valor", "FechaModificacion"); errTr != nil {
+									err = errTr
+									fmt.Println(err)
+									_ = o.Rollback()
+									return
+								}
+							} else {
+								v3.SolicitudId = v2.PaqueteSolicitud.SolicitudId
+								if _, errTr = o.Insert(&v2); errTr != nil {
+									err = errTr
+									fmt.Println(err)
+									_ = o.Rollback()
+									return
+								}
 							}
 						} else {
-							v3.SolicitudId = v2.PaqueteSolicitud.SolicitudId
-							if _, errTr = o.Insert(&v2); errTr != nil {
-								err = errTr
-								fmt.Println(err)
-								_ = o.Rollback()
-								return
-							}
+							err = errTr
+							fmt.Println(err)
+							_ = o.Rollback()
+							return
 						}
-					} else {
-						err = errTr
-						fmt.Println(err)
-						_ = o.Rollback()
-						return
 					}
-				}
-*/	
+				*/
 			}
 		}
 	} else {
@@ -288,15 +289,15 @@ func UpdatePaquete(m *TrPaquete) (err error) {
 	return
 }
 
-func GetAllPaquetes()(v []interface{}, err error){
+func GetAllPaquetes() (v []interface{}, err error) {
 	o := orm.NewOrm()
 
 	var paquetes []*Paquete
 	if _, err := o.QueryTable(new(Paquete)).RelatedSel().Filter("Activo", true).All(&paquetes); err == nil {
 		for _, paquete := range paquetes {
 			v = append(v, map[string]interface{}{
-				"Id":                    paquete.Id,
-				"Nombre":                paquete.Nombre,
+				"Id":     paquete.Id,
+				"Nombre": paquete.Nombre,
 			})
 		}
 		return v, nil
