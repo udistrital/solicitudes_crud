1c2052721f10637246f5b4694039854c62290264
Jose Luis Quintero C <jlqc1772@gmail.com>
2022-01-15 03:33:04 -0500

    Revert "management: Insert de solicitudes para derechos pecuniarios "

--- database/scripts/20220115_025328_agregar_solicitud_derechos_pecuniarios_down.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM solicitud.estado WHERE tipo_solicitud_id = (SELECT id FROM solicitud.tipo_solicitud WHERE codigo_abreviacion = 'SolDerPec');
-
-DELETE FROM solicitud.tipo_solicitud WHERE codigo_abreviacion = 'SolDerPec';
